6a0affe35bdcb9b56c043fbfd157a9f1589f35d2
vairamuthu1 <41635232+vairamuthu1@users.noreply.github.com>
2020-02-05 07:16:20 +0530

    openshift test
    openshift test

---
+++ SpringBootHelloWorld/src/main/java/com/javainuse/controllers/TestController.java
@@ -0,0 +1,14 @@
+package com.javainuse.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.RestController;  
+@RestController 
+public class TestController {
+
+	@RequestMapping("/hello")  
+    public String hello(){  
+        return"Hello springboot!";  
+	}
+
+}
